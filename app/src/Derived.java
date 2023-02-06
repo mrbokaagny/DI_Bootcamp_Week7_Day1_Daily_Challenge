402bf86c6408d16fa25b8ae82b739e7c00a2c185
mrdevbanks <103212600+mrdevbanks@users.noreply.github.com>
2023-02-06 09:15:12 +0000

    projet fini

---
+++ app/src/Derived.java
@@ -0,0 +1,8 @@
+import com.di.dailychallenge.classes.Base;
+
+public class Derived extends Base {
+    public static void main(String[] args) throws Exception {
+    Base base = new Base();
+    base.Salutation();
+    }
+}
---
+++ app/src/com/di/dailychallenge/classes/Base.java
@@ -0,0 +1,11 @@
+package com.di.dailychallenge.classes;
+
+public class Base {
+    
+    public  Base(){}
+
+    public void Salutation(){
+        System.out.println("Hey !!! Salut 😊 ....");
+    }
+    
+}
